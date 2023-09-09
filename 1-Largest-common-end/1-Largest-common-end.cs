c0b86be4ffb2f0add96705b435ca2523cd407242
Savina Daria <savina.dariia@chnu.edu.ua>
2023-09-09 16:41:11 +0300

    tasks 1 and 2

---
+++ 1-Largest-common-end/1-Largest-common-end.cs
@@ -0,0 +1,40 @@
+﻿string line1 = Console.ReadLine();
+string line2 = Console.ReadLine();
+string[] first = line1.Split(" ");
+string[] second = line2.Split(" ");
+int length = (first.Length < second.Length) ? (first.Length) : (second.Length);
+int counter_start = -1;
+for (int i = 0; i < length; i++)
+{
+    if (first[i].Equals(second[i]))
+    {
+        counter_start++;
+    }
+    else
+    { break; }
+}
+int counter_end = -1;
+for (int i = length - 1; i > length; i--)
+{
+    if (first[i].Equals(second[i]))
+    {
+        counter_end++;
+    }
+    else
+    { break; }
+}
+
+if (counter_start != -1)
+{
+    for (int i = 0; i < counter_start; i++)
+        Console.WriteLine(first[i]);
+}
+if (counter_end != 1)
+{
+    for (int i = length - 1; i > (length - 1 - counter_end); i--)
+        Console.WriteLine(first[i]);
+}
+if (counter_end==-1 && counter_start == -1)
+{
+    Console.WriteLine("none");
+}
---
+++ 2-rotate-and-sum/2-rotate-and-sum.cs
@@ -0,0 +1,30 @@
+﻿string array = Console.ReadLine();
+string[] initial = (array.Split(" "));
+int k = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[initial.Length];
+
+for (int i = 0; i < initial.Length; i++)
+{
+    numbers[i] = Convert.ToInt32(initial[i]);
+}
+
+int[] result = new int[numbers.Length];
+
+for (int i = 0;i < numbers.Length; i++)
+{
+    int rotation_index = i;
+    for (int counter=0; counter<k; counter++)
+    {
+        rotation_index--;
+        if (rotation_index < 0)
+        {
+            rotation_index = numbers.Length + rotation_index;
+        }
+        result[i] += numbers[rotation_index];
+    }
+}
+
+foreach  (int i in result)
+{
+    Console.WriteLine(i);
+}
